e2a29b5e9cce4a257c03bddf1f1788c043f75728
Marco Antonio Humberto Decormis Pinto <nesdufrik@gmail.com>
2024-09-26 00:26:30 -0400

    Casa Avance

--- src/App.vue
+++ src/App.vue
@@ -1,117 +1,78 @@
 <template>
-	<div class="container">
-		<h1>Reservas Museo del Chocolate</h1>
-		<div>
-			<table class="table">
-				<thead>
-					<tr>
-						<th scope="col">Fecha</th>
-						<th scope="col">Cliente</th>
-						<th scope="col">Total</th>
-						<th scope="col">Acciones</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr v-for="reserva in reservas" :key="reserva.identificador">
-						<th scope="row">{{ reserva.programacion.fecha }}</th>
-						<td>{{ reserva.cliente.nombre }}</td>
-						<td>{{ reserva.pago.total }}</td>
-						<td>
-							<button
-								type="button"
-								class="btn btn-primary btn-sm"
-								data-bs-toggle="modal"
-								data-bs-target="#exampleModal"
-								@click="verReserva(reserva.identificador)"
-							>
-								Ver
-							</button>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
-	<!-- Modal -->
-	<div
-		class="modal fade"
-		id="exampleModal"
-		tabindex="-1"
-		aria-labelledby="exampleModalLabel"
-		aria-hidden="true"
-		data-bs-backdrop="static"
-	>
-		<div class="modal-dialog modal-lg">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h1 class="modal-title fs-5" id="exampleModalLabel">
-						Confirmación de Pago
-					</h1>
-					<button
-						type="button"
-						class="btn-close"
-						data-bs-dismiss="modal"
-						aria-label="Close"
-					></button>
+	<div class="container mx-auto px-4">
+		<DataTable :value="reservas" tableStyle="min-width: 50rem">
+			<template #header>
+				<div class="flex flex-wrap items-center justify-between gap-2">
+					<span class="text-xl font-bold">RESERVAS PARA CONFIRMAR</span>
+					<Button icon="pi pi-refresh" rounded raised />
 				</div>
-				<div class="modal-body">
-					<div class="d-flex flex-row">
-						<div class="col-6">
-							<img
-								:src="reservaFiltrada?.pago.comprobante"
-								alt=""
-								class="img-fluid"
-							/>
-						</div>
-						<div class="col-6">
-							<table class="table">
-								<tbody>
-									<tr
-										v-for="(tipo, index) in reservaFiltrada?.cantidad"
-										:key="index"
-									>
-										<td>{{ tipo?.tipo }}</td>
-										<td>{{ tipo?.cantidad }}</td>
-									</tr>
-									<tr>
-										<td>Total</td>
-										<td>
-											{{ reservaFiltrada?.cantidadTotal }}
-										</td>
-									</tr>
-									<tr>
-										<td>Pago Total</td>
-										<td>{{ reservaFiltrada?.pago.total }}</td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
-					</div>
-				</div>
-				<div class="modal-footer">
-					<button
+			</template>
+			<Column header="Fecha Registro">
+				<template #body="slotProps">
+					{{
+						format(
+							new Date(slotProps.data.fechaRegistro),
+							{ date: 'short', time: 'short' },
+							'es'
+						)
+					}}
+				</template>
+			</Column>
+			<Column field="cliente.nombre" header="Nombre"></Column>
+			<Column field="programacion.fecha" header="Día de reserva">
+				<template #body="slotProps">
+					{{
+						format(new Date(slotProps.data.programacion.fecha), 'medium', 'es')
+					}}
+				</template>
+			</Column>
+			<Column
+				field="cantidadTotal"
+				header="Personas"
+				headerStyle="width: 5rem; text-align: center"
+				class="text-center"
+			></Column>
+			<Column
+				headerStyle="width: 5rem; text-align: center"
+				bodyStyle="text-align: center; overflow: visible"
+			>
+				<template #body="slotProps">
+					<Button
 						type="button"
-						class="btn btn-secondary"
-						data-bs-dismiss="modal"
-					>
-						Cancelar
-					</button>
-					<button
-						type="button"
-						class="btn btn-success"
-						@click="confirmarReserva"
-						:disabled="loading"
-					>
-						Confirmar
-					</button>
+						icon="pi pi-eye"
+						rounded
+						severity="secondary"
+						@click="verReserva(slotProps.data.identificador)"
+					/>
+				</template>
+			</Column>
+		</DataTable>
+		<Dialog
+			v-model:visible="visible"
+			modal
+			header="Comprobación de pago"
+			:style="{ width: '90vw' }"
+			:breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+		>
+			<div class="grid grid-cols-1 md:grid-cols-2">
+				<div class="text-center md:text-start">
+					<Image
+						:src="reservaFiltrada.pago.comprobante"
+						alt="Image"
+						width="250"
+						preview
+					/>
 				</div>
+				<div>Aqui vendran detalles</div>
 			</div>
-		</div>
+		</Dialog>
 	</div>
 </template>
 <script setup>
 import { ref, onMounted } from 'vue'
+import { format } from '@formkit/tempo'
 
+const visible = ref(false)
 const reservas = ref([])
 const reservaFiltrada = ref(null)
 const loading = ref(false)
@@ -121,25 +82,22 @@ onMounted(async () => {
 })
 
 const traerReservas = async () => {
-	const res = await fetch('https://n8ntest.friktek.com/webhook/confirmaciones')
+	const res = await fetch('https://n8n.friktek.com/webhook/confirmaciones')
 	const data = await res.json()
 	return data.data
 }
 
 const confirmarReserva = async () => {
 	loading.value = true
-	const res = await fetch(
-		'https://n8ntest.friktek.com/webhook/confirmaciones',
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				identificador: reservaFiltrada.value.identificador,
-			}),
-		}
-	)
+	const res = await fetch('https://n8n.friktek.com/webhook/confirmaciones', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			identificador: reservaFiltrada.value.identificador,
+		}),
+	})
 	loading.value = false
 	const data = await res.json()
 	console.log(data)
@@ -156,6 +114,7 @@ const verReserva = (id) => {
 	reservaFiltrada.value = reservas.value.find(
 		(reserva) => reserva.identificador === id
 	)
+	visible.value = true
 }
 </script>
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle'
-
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import ConfirmationService from 'primevue/confirmationservice'
+import Aura from '@primevue/themes/aura'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './assets/main.css'
+import 'primeicons/primeicons.css'
+
+const app = createApp(App)
+app.use(ToastService)
+app.use(ConfirmationService)
+app.use(PrimeVue, {
+	theme: {
+		preset: Aura,
+		options: {
+			cssLayer: {
+				name: 'primevue',
+				order: 'tailwind-base, primevue, tailwind-utilities',
+			},
+			darkModeSelector: '.dark-mode',
+		},
+	},
+})
+app.mount('#app')
