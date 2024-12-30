2266921303e8161f14f25ad60c172ed0731f6944
solactiva <adeveloper236@gmail.com>
2024-12-30 15:27:14 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssPrimeui from 'tailwindcss-primeui'
 export default {
 	content: [
 		'./index.html',
@@ -8,5 +9,5 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [require('tailwindcss-primeui')],
+	plugins: [tailwindcssPrimeui],
 }
